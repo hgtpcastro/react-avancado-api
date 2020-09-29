eb5d2062c2515e9e97e6e863b9087123e5a138fa
Hugo Castro Costa <hg.tp.castro@gmail.com>
2020-09-29 14:42:50 -0300

    Initial commit from Create Strapi App

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'af66040e7612351e564be2de168a26eb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '62d866f8-48be-4abc-bd16-ed5782ea0cbc'
+};
